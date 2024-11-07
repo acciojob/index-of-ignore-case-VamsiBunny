896e64ac78daedc62958321bd6277e0c5c0cab26
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-07 15:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let a=s1.toLowerCase();
+	let b=s2.toLowerCase();
+	return a.indexOf(b);
 }
 
-// Please do not change the code below
+// // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
